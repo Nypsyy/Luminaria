0504b0a1097f545029fe22cb4be823dc7ded5df9
Nypsyy <mayeul.marsaut@hotmail.fr>
2020-11-19 18:18:35 +0100

    Parallax

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [SerializeField] GameObject cam;
+    [SerializeField] float parallaxEff;
+    float length, startPos;
+
+    void Start()
+    {
+        startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void Update()
+    {
+        float tmp = cam.transform.position.x * (1 - parallaxEff);
+        float dist = cam.transform.position.x * parallaxEff;
+
+        transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
+
+        if (tmp > startPos + length)
+            startPos += length;
+        else if (tmp < startPos - length)
+            startPos -= length;
+    }
+}
