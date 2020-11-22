7b4debaaa0c4b5a04b1281b35b1026fbd56a7380
Nypsyy <mayeul.marsaut@hotmail.fr>
2020-11-22 18:26:29 +0100

    Health bar + damage taking on collision

---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class PlayerCharacter : MonoBehaviour
+{
+    public int maxHealth;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
+    bool invincible;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(currentHealth);
+        Debug.Log(healthBar.slider.value);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (!invincible)
+        {
+            currentHealth -= damage;
+            healthBar.SetHealth(currentHealth);
+            StartCoroutine(Invincible());
+        }
+    }
+
+    IEnumerator Invincible()
+    {
+        invincible = true;
+        yield return new WaitForSeconds(1);
+        invincible = false;
+    }
+
+
+}
